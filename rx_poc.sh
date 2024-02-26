2b54f2c98bb459b66650bd9d7412aaf48d6596e7
Nasr Hanslo <nasrhanslo@google.com>
2024-02-26 15:33:13 +0200

    additional code which: gets current time on local machine, and combines the different variable with current and forecasted temp and logs it to the weather log file : rx_poc.log, also cron scheduling to submit temperature forecasted for the next day at noon for given city, scheduled for noon relative to given city

--- rx_poc.sh
+++ rx_poc.sh
@@ -7,11 +7,24 @@ curl -s wttr.in/casablanca?T --output weather_report
 obs_temp=$(curl -s wttr.in/casablanca?T | grep -E -m 1 '°.' | grep -Eo -e '-?[[:digit:]].*')
 
 #To extract Current Temperature
-obs_temp=$(curl -s wttr.in/$city?T | grep -m 1 '°.' | grep -Eo -e '-?[[:digit:]].*')
-echo "The current Temperature of $city: $obs_temp"
+obs_temp=$(curl -s wttr.in/$city?T | grep -m 1 '°.' | grep -Eo -e '-?[[:digit:]].*' | cut -c 1,2)
+echo "The current Temperature of $city is: $obs_temp °C"
 
 
-fc_temp=$(curl -s wttr.in/casablanca?T | head -n 23 | tail -1 |  grep '°.' | cut -d "C" -f 2 | grep -Eo -e '-?[[:digit:]]')
+fc_temp=$(curl -s wttr.in/casablanca?T | head -n 23 | tail -1 |  grep '°.' | cut -d "C" -f 2 | grep -Eo -e '-?[[:digit:]].*' | cut -c 1,2)
 
-echo $fc_temp
+echo "the forecasted Temperature of the city is: $fc_temp °C"
 
+hour=$(TZ=Casablanca date '+ %H')
+day=$(TZ=Casablanca date '+ %d')
+month=$(TZ=Casablanca date '+ %m')
+year=$(TZ=Casablanca date '+ %Y')
+
+current=$(echo -e "$year\t$month\t$day\t$obs_temp\t$fc_temp")
+
+echo $current >>  rx_poc.log
+
+
+cityTime=$(TZ="$city" date)
+
+echo $cityTime
